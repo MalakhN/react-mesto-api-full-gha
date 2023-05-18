23269a51b120ec8b3b5029f9ae2e0978548cb126
MalakhN <nata.malakh89@yandex.ru>
2023-05-18 19:56:24 +0300

    Mesto Russia (sprint 16): fix after 2 review

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -22,7 +22,7 @@ const getUserById = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь не найден');
       } else {
-        res.send(user);
+        res.send({ data: user });
       }
     })
     .catch((err) => {
@@ -40,7 +40,7 @@ const getUserInfo = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь не найден');
       } else {
-        res.send(user);
+        res.send({ data: user });
       }
     })
     .catch((err) => {
@@ -59,7 +59,7 @@ const createUser = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => User.create({
-      email, password: hash, name, about, avatar
+      name, about, avatar, email, password: hash
     }))
     .then((newUser) => {
       res.status(201).send({
@@ -124,7 +124,7 @@ const updateProfile = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь с данным id не найден');
       } else {
-        res.send(user);
+        res.send({ data: user });
       }
     })
     .catch((err) => {
@@ -151,7 +151,7 @@ const updateAvatar = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь с данным id не найден');
       } else {
-        res.send(user);
+        res.send({ data: user });
       }
     })
     .catch((err) => {
